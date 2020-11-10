a9ba6e6b09728983f6c933195964b824129d0abb
GaYoung <kians2@naver.com>
2020-11-10 14:56:43 +0900

    new

--- RecordServer/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : `mongodb+srv://gyoung:0000@cluster0.kebbe.gcp.mongodb.net/<recordApp>?retryWrites=true&w=majority`
-}
--- RecordServer/config/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : process.env.mongoURI
-}
